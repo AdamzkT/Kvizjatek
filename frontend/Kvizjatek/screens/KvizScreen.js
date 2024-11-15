cfc853c93d3d4760b419193a8f6cca03a6906282
AdamzkT <adamszki.tamas.1029@dszcbaross.edu.hu>
2024-11-15 10:17:11 +0100

    no_random commit

--- frontend/Kvizjatek/screens/KvizScreen.js
+++ frontend/Kvizjatek/screens/KvizScreen.js
@@ -1,4 +1,4 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 import { Ipcim1 } from '../Ipcim';
 import { Ipcim2 } from '../Ipcim';
 import { useEffect, useState } from 'react';
@@ -14,6 +14,7 @@ export default function KvizScreen() {
     let y = await x.json()
     setAdatok(y)
     setKerdes(adatok[0].kerdes)
+    setValaszok([adatok[0].valasz_jo, adatok[0].valasz_rossz1, adatok[0].valasz_rossz2, adatok[0].valasz_rossz3])
     setJoValasz(adatok[0].valasz_jo)
   }
 
@@ -22,14 +23,21 @@ export default function KvizScreen() {
   const ujKerdes = () => {
     let ujIndex = Math.floor(Math.random() * adatok.length)
     setKerdes(adatok[ujIndex].kerdes)
+    setValaszok([adatok[ujIndex].valasz_jo, adatok[ujIndex].valasz_rossz1, adatok[ujIndex].valasz_rossz2, adatok[ujIndex].valasz_rossz3])
     setJoValasz(adatok[ujIndex].valasz_jo)
+  }
+
+  const valaszEllenorzes = (valasz) => {
+    if(valasz == joValasz) { Alert.alert('','Jó válasz') }
+    else { Alert.alert('','Rossz válasz')}
+    ujKerdes()
   }
 
   return (
     <View style={styles.container}>
       <Button title='Új kérdés' onPress={() => ujKerdes()}/>
       <Text>{kerdes}</Text>
-      <Text>{joValasz}</Text>
+      {valaszok.map((valasz, k) =><Button key={k} title={valasz} onPress={() => valaszEllenorzes(valasz)}/>)}
     </View>
   );
 }
