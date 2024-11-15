ad9032a4be458bb45567e2bf46594cd6e2ad0540
AdamzkT <adamszki.tamas.1029@dszcbaross.edu.hu>
2024-11-15 11:10:05 +0100

    random sorrend commit

--- frontend/Kvizjatek/screens/KvizScreen.js
+++ frontend/Kvizjatek/screens/KvizScreen.js
@@ -9,21 +9,38 @@ export default function KvizScreen() {
   const [valaszok,setValaszok] = useState([]);
   const [joValasz,setJoValasz] = useState("");
 
+  const keveres = (tomb) => {
+    let sorrend = []
+
+    while(sorrend.length < tomb.length){
+      let r = Math.floor(Math.random() * tomb.length)
+      if(!sorrend.includes(r)) { sorrend.push(r) }
+    }
+
+    let valasz_1 = tomb[sorrend[0]]
+    let valasz_2 = tomb[sorrend[1]]
+    let valasz_3 = tomb[sorrend[2]]
+    let valasz_4 = tomb[sorrend[3]]
+    setValaszok([valasz_1, valasz_2, valasz_3, valasz_4])
+  }
+
   const lekerdez = async () => {
     let x = await fetch(`${Ipcim1}/kerdesek`)
     let y = await x.json()
     setAdatok(y)
-    setKerdes(adatok[0].kerdes)
-    setValaszok([adatok[0].valasz_jo, adatok[0].valasz_rossz1, adatok[0].valasz_rossz2, adatok[0].valasz_rossz3])
-    setJoValasz(adatok[0].valasz_jo)
+    setKerdes(y[0].kerdes)
+    keveres([y[0].valasz_jo, y[0].valasz_rossz1, y[0].valasz_rossz2, y[0].valasz_rossz3])
+    setJoValasz(y[0].valasz_jo)
   }
 
-  useEffect(() => {lekerdez()},[])
+  useEffect(() => {
+    lekerdez()
+  },[])
 
-  const ujKerdes = () => {
+  const ujKerdes = async () => {
     let ujIndex = Math.floor(Math.random() * adatok.length)
     setKerdes(adatok[ujIndex].kerdes)
-    setValaszok([adatok[ujIndex].valasz_jo, adatok[ujIndex].valasz_rossz1, adatok[ujIndex].valasz_rossz2, adatok[ujIndex].valasz_rossz3])
+    keveres([adatok[ujIndex].valasz_jo, adatok[ujIndex].valasz_rossz1, adatok[ujIndex].valasz_rossz2, adatok[ujIndex].valasz_rossz3])
     setJoValasz(adatok[ujIndex].valasz_jo)
   }
 
@@ -35,9 +52,9 @@ export default function KvizScreen() {
 
   return (
     <View style={styles.container}>
-      <Button title='Új kérdés' onPress={() => ujKerdes()}/>
+      <Button title='Új kérdés' onPress={async () => ujKerdes()}/>
       <Text>{kerdes}</Text>
-      {valaszok.map((valasz, k) =><Button key={k} title={valasz} onPress={() => valaszEllenorzes(valasz)}/>)}
+      {valaszok.map((valasz, k) => <Button key={k} title={valasz} onPress={() => valaszEllenorzes(valasz)}/>)}
     </View>
   );
 }
