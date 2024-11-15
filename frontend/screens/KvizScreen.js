6a06915bf23ef09698f5263ef8675a0ee4563596
AdamzkT <adamszki.tamas.1029@dszcbaross.edu.hu>
2024-11-15 09:28:27 +0100

    ipcim commit

---
+++ frontend/screens/KvizScreen.js
@@ -0,0 +1,23 @@
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+const lekerdez = async () =>
+{
+    
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Button>AAA</Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
