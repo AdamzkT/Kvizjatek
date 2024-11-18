46cd7236eff297b944930eb0208676ac225f6514
ZahonyiBenjamin <zahonyi.benjamin.1055@dszcbaross.edu.hu>
2024-11-18 10:48:18 +0100

    database update and images

--- backend/backend.js
+++ backend/backend.js
@@ -42,6 +42,75 @@ app.get('/kerdesek', (req, res) => {
 })
 
 
+app.get('/kvizek', (req, res) => {
+    kapcsolat()
+
+    connection.query(`
+        SELECT * FROM kvizek
+        INNER JOIN kerdesek ON kvizek.id = kerdesek.kviz_id
+        `, (err, rows, fields) => {
+        if (err)
+        {
+            console.log("Hiba")
+            console.log(err)
+            res.status(500).send("Hiba")
+        }
+        else{
+            console.log(rows)
+            res.status(200).send(rows)
+        }
+    })
+
+    connection.end() 
+})
+
+
+app.get('/felhasznalok', (req, res) => {
+    kapcsolat()
+
+    connection.query(`
+        SELECT * FROM felhasznalok
+        `, (err, rows, fields) => {
+        if (err)
+        {
+            console.log("Hiba")
+            console.log(err)
+            res.status(500).send("Hiba")
+        }
+        else{
+            console.log(rows)
+            res.status(200).send(rows)
+        }
+    })
+
+    connection.end() 
+})
+
+
+app.get('/osszes_tabla', (req, res) => {
+    kapcsolat()
+
+    connection.query(`
+        SELECT * FROM kvizek
+        INNER JOIN kerdesek ON kvizek.id = kerdesek.kviz_id
+        INNER JOIN felhasznalok ON kvizek.felhasznalo_email = felhasznalok.email
+        `, (err, rows, fields) => {
+        if (err)
+        {
+            console.log("Hiba")
+            console.log(err)
+            res.status(500).send("Hiba")
+        }
+        else{
+            console.log(rows)
+            res.status(200).send(rows)
+        }
+    })
+
+    connection.end() 
+})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
