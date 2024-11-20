845aff2dbe7abd062f2863282a7d1f519c87c395
AdamzkT <adamszki.tamas.1029@dszcbaross.edu.hu>
2024-11-20 08:41:26 +0100

    kapcsolatScreen commit

--- frontend/App.js
+++ frontend/App.js
@@ -3,14 +3,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import KvizScreen from './screens/KvizScreen'
+import KapcsolatScreen from './screens/KapcsolatScreen';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Kviz">
-        <Drawer.Screen name="Kviz" component={KvizScreen} />
+      <Drawer.Navigator initialRouteName="Kvíz">
+        <Drawer.Screen name="Kvíz" component={KvizScreen} />
+        <Drawer.Screen name="Kapcsolat" component={KapcsolatScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
