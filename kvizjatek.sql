8f4829a6bd6585c143a233a3bf958e97b4d5ecea
ZahonyiBenjamin <zahonyi.benjamin.1055@dszcbaross.edu.hu>
2024-11-20 08:22:16 +0100

    database with actual database

--- kvizjatek.sql
+++ kvizjatek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Nov 20. 08:18
+-- Létrehozás ideje: 2024. Nov 20. 08:21
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.2.4
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Adatbázis: `kvizjatek`
 --
+CREATE DATABASE IF NOT EXISTS `kvizjatek` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+USE `kvizjatek`;
 
 -- --------------------------------------------------------
 
