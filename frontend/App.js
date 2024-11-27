b983333bfedc60ae8d287c9d4ef37949f81db3e0
AdamzkT <adamszki.tamas.1029@dszcbaross.edu.hu>
2024-11-27 09:18:33 +0100

    AsyncStorage first implementation, needs fixing

--- frontend/App.js
+++ frontend/App.js
@@ -1,33 +1,12 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createStackNavigator } from '@react-navigation/stack';
-import KvizScreen from './screens/KvizScreen'
-import KapcsolatScreen from './screens/KapcsolatScreen';
-import BejelentkezesScreen from './screens/BejelentkezesScreen';
-import RegisztracioScreen from './screens/RegisztracioScreen';
-
-const Drawer = createDrawerNavigator();
-const Stack = createStackNavigator()
-
-function BejelentkezesStack() {
-  return (
-    <Stack.Navigator initialRouteName="Bejelentkezés">
-      <Stack.Screen name="Bejelentkezés" component={BejelentkezesScreen} options={{ headerShown: false }}/>
-      <Stack.Screen name="Regisztráció" component={RegisztracioScreen} />
-    </Stack.Navigator>
-  );
-}
+import { NavigationContainer } from '@react-navigation/native';
+import DrawerScreen from './DrawerScreen';
 
 export default function App() {
+
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Kvíz">
-        <Drawer.Screen name="Kvíz" component={KvizScreen} />
-        <Drawer.Screen name="Kapcsolat" component={KapcsolatScreen} />
-        <Drawer.Screen name="BejelentkezesStack" component={BejelentkezesStack} options={{ title: 'Bejelentkezés' }}/>
-      </Drawer.Navigator>
+      < DrawerScreen />
     </NavigationContainer>
   );
 }
