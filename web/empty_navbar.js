e9d2959179d90ddf1d5711f95ebe61e477074a50
ZahonyiBenjamin <zahonyi.benjamin.1055@dszcbaross.edu.hu>
2025-04-04 13:03:10 +0200

    logo

---
+++ web/empty_navbar.js
@@ -0,0 +1,16 @@
+let navbar = `
+<nav class="navbar-expand-sm nav_design fixed-top">
+    <div class="container-fluid d-flex align-items-center">
+        <span class="navbar-brand logo_name">Pocket Quiz</span>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+              
+            </ul>
+        </div>
+    </div>
+</nav>
+`
+document.getElementById("navbar_doboz").innerHTML = navbar;
--- web/navbar.js
+++ web/navbar.js
@@ -1,31 +1,30 @@
 let navbar = `
 <nav class="navbar-expand-sm nav_design fixed-top">
-    <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="kvizek.html">Kvízek</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="kategoriak.html">Kategóriák</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="visszajelzesek.html">Visszajelzések</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="informacio.html">Információ</a>
-            </li>
-
-
-            <li class="nav-item">
-                <a class="nav-link nav_kijelentkezes" href="#" id="kijelentkezes">Kijelentkezés</a>
-            </li>
-        </ul>
-    </div>
+    <div class="container-fluid d-flex align-items-center">
+        <span class="navbar-brand logo_name">Pocket Quiz</span>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="kvizek.html">Kvízek</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="kategoriak.html">Kategóriák</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="visszajelzesek.html">Visszajelzések</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="informacio.html">Információ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav_kijelentkezes" href="#" id="kijelentkezes">Kijelentkezés</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
 `
-document.getElementById("navbar_doboz").innerHTML = navbar
+document.getElementById("navbar_doboz").innerHTML = navbar;
